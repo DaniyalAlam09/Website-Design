822e5769484a926105632db855d1d6bd4785be92
Daniyal Alam <90774564+DaniyalAlam09@users.noreply.github.com>
2022-09-09 00:31:43 +0500

    Bootstrap Complex Layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/calc-bg.svg" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/anno-bg.svg" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/anno-bg.svg" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/breaking-bg.svg" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/ExecutiveUpdate.svg" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/ExecutiveUpdate.svg" alt="" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-6 col-md-8 col-sm-6">
+            <div className="row">
+              <div className="col-lg-4 col-md-6 col-sm-12">
+                <img src="images/account-bg1.svg" alt="" />
+              </div>
+              <div className="col-lg-4 col-md-6 col-sm-12">
+                <img src="images/Audit.svg" alt="" />
+              </div>
+              <div className="col-lg-4 col-md-6 col-sm-12">
+                <img src="images/BioMedVentures.svg" alt="" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-4 col-md-6 col-sm-12">
+                <img src="images/development-bg.svg" alt="" />
+              </div>
+              <div className="col-lg-4 col-md-6 col-sm-12">
+                <img src="images/finance-bg.svg" alt="" />
+              </div>
+              <div className="col-lg-4 col-md-6 col-sm-12">
+                <img src="images/homepage.png" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-8 col-sm-12 big">
+            <img src="images/quick-launch-bg.svg" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-12">
+            <div className="">
+              <img src="images/PressRelease.svg" alt="" />
+            </div>
+            <div className="">
+              <img src="images/enews-bg.svg" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/HumanResources.svg" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/Investments-bg.png" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/it-bg.svg" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/leasing-bg.svg" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/legal-bg.svg" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/marketing-bg.svg" alt="" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-1 col-md-4 col-sm-6">
+            <div className="">
+              <img src="images/smile-bg.png" alt="" />
+            </div>
+            <div className="">
+              <img src="images/phone-bg.png" alt="" />
+            </div>
+          </div>
+          <div className="col-lg-1 col-md-4 col-sm-6">
+            <div className="">
+              <img src="images/page-bg.png" alt="" />
+            </div>
+            <div className="">
+              <img src="images/phone-bg.png" alt="" />
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="row">
+              <div className="col">
+                <img src="images/box-bg.png" alt="" />
+              </div>
+              <div className="col">
+                <img src="images/tweet-bg.svg" alt="" />
+              </div>
+              <div className="col">
+                <img src="images/homepage.png" alt="" />
+              </div>
+            </div>
+            <div className="col">
+              <img src="images/LeedershipIsLighting.jpg" alt="" />
+            </div>
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/Operations.svg" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/Risk-Mgmt.jpg" alt="" />
+          </div>
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <img src="images/EmplyoeeConnect1.png" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
